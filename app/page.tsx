513813a62037ae40a62c7364a2c03e5f50667f2b
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-02-04 01:45:12 +0500

    Code structure changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Layout } from "./components/layout/Layout";
 import { Music } from "./components/music/Music";
+import { Work } from "./components/work/Work";
 
 export default function Home() {
   return (
     <>
     <Layout/>
     <Music/>
+    <Work/>
     </>
   );
 }
