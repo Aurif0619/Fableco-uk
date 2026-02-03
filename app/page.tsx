280a4a0448252012c89e6726229c91efaec3f820
Aurif0619 <muhammadaurifsolangixwave@gmail.com>
2026-02-04 01:41:32 +0500

    Created folder structure and write a code with new way

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Layout } from "./components/layout/Layout";
+import { Music } from "./components/music/Music";
 
 export default function Home() {
   return (
     <>
     <Layout/>
+    <Music/>
     </>
   );
 }
